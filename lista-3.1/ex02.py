e22b1b5c3331d66b5d07b1815c6f30ed09c79c4c
GKanjiscuk <gi.kanjiscuk@gmailcom>
2024-08-20 11:27:55 -0300

    mod ex02

--- lista-3.1/ex02.py
+++ lista-3.1/ex02.py
@@ -2,13 +2,16 @@ valor = int(input('Digite o valor a ser pago: '))
 pagamento = int(input('Digite o pagamento efetuado: '))
 notas = [50, 20, 10, 5, 2, 1]
 troco = pagamento - valor
-trcCliente = []
+trcCliente = {}
 
 for nota in notas:
+    quantidade = 0
     while troco >= nota:
-        trcCliente.append(nota)
         troco = troco - nota
+        quantidade += 1
+        if (quantidade > 0):
+            trcCliente[nota] = quantidade
 
-  
-print(f'Troco: {trcCliente} ')
-
+print('Troco:')
+for nota, quantidade in trcCliente.items():
+    print(f'{quantidade} nota(s) de R$ {nota}')
